8a587fc3a46b92e5a07c630db2c89488e44eb889
Ahmed Essam El Sayed <ahmedessaam124@gmail.com>
2024-12-02 03:21:15 +0200

    0000000000

--- src/MainComponents/Footer/Footer.tsx
+++ src/MainComponents/Footer/Footer.tsx
@@ -1,9 +1,70 @@
-
-const Footer = () => {
+import { Link, useNavigate } from "react-router-dom";
+import style from "./Footer.module.css"
+import iconImage from "../../../public/favIcon.png"
+function Footer() {
+const navicate = useNavigate();
+const goHome = () => {
+    navicate("/")
+}
     return (
+    
         <>
-        <h1>Footer</h1>
+        <footer>
+
+<div className={style.FirstHalfFooter}>
+
+
+<span className={style.IconTittle}>
+        <img src={iconImage} alt=""/>
+    <h1 onClick={goHome} className={style.fontTiltle}>PurelyTasty </h1>
+    </span>
+
+
+<div className={style.contentLinks}>
+
+    <span className={style.oneRowLinksFooter}>
+<Link className={style.decorationnone} to={""}>Library</Link>
+<Link className={style.decorationnone} to={"/account"}>About US</Link>
+<Link className={style.decorationnone} to={"/contactus"}>Contact Us</Link>
+</span>
+
+<span className={style.twoRowLinksFooter}>
+<Link className={style.decorationnone} to={""}>Services</Link>
+<Link className={style.decorationnone} to={"/account"}>Account</Link>
+</span>
+
+
+</div>
+
+
+</div>
+
+
+
+<hr />
+
+
+
+<div className={style.SecondHalfFooter}>
+<p>At GreenNest, we are committed to empowering farmers and plant enthusiasts with cutting-edge technology to ensure healthier crops and a sustainable future. Simply upload a photo of your plant, and our advanced diagnostic tool will identify potential diseases and offer tailored solutions. With our mobile app and Join us to use our site to discover your plant, you can access this innovative service anytime, anywhere. Join us in cultivating healthier plants and stronger communities today!" Feel free to customize the tone or details to better fit your project. Let me know if you'd like help refining this further!
+</p>
+
+<span className={style.imgGooglePlay}>
+    <img src={""} />
+    </span>
+
+</div>
+<hr/>
+
+<div className={style.thirdHalfFooter}>
+<p>© 2024 PurelyTasty
+</p>
+
+</div>
+
+
+   </footer>
         </>
     );
-}
-export default Footer ;
+    }
+    export default Footer ;
--- src/MainComponents/Header/Header.tsx
+++ src/MainComponents/Header/Header.tsx
@@ -1,8 +1,117 @@
-
+import style from "./Header.module.css"
+import "./Header.css"
+import iconImage from "../../../public/favIcon.png"
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
+
+
+
+    const navicate = useNavigate();
+
+    function goHome()  {
+      navicate("/")
+    }
+    
     return (
         <>
-        <h1>Head</h1>
+
+
+    <nav>
+  
+<div className={style.leftSide}>
+
+{/* ============================================================================================================================= */}
+{/* ============================================================================================================================= */}
+
+<span className={style.HideInNaturalState}>
+<button className="btn listSides " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+<FontAwesomeIcon className="fontAwsemList"  icon={faBars} />
+</button>
+
+<div className="offcanvas offcanvas-start" tabIndex={-1} id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div className="offcanvas-header">
+
+  <span  onClick={goHome} className={style.IconTittle}>
+        <img src={iconImage}/>
+    <h1 className="fontTiltle">PurelyTasty </h1>
+    </span>
+
+    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div className="offcanvas-body">
+   
+
+  <div className={style.ilnksContents
+}>
+<Link className={style.linkNavs} to={""}>Library</Link>
+<Link className={style.linkNavs} to={"/contactus"}>Contact Us</Link>
+<Link className={style.linkNavs} to={"/account"}>Account</Link>
+
+<div className="dropdown d-block">
+  <button className="btn secondbtnf  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  Restaurant
+  </button>
+  <ul className="dropdown-menu ">
+    <li><a className="dropdown-item " href="#">Services</a></li>
+    <li><Link className="dropdown-item" to={"/aboutus"}>About Us</Link></li>
+  </ul>
+</div>
+
+
+</div>
+
+{/* <div className={style.listdisplayBuuton}> */}
+    <button className={style.btnGetAppp}>Get The APP</button>
+
+
+     
+  </div>
+</div>
+</span>
+
+{/* =================================================================================================================================
+=========================================================================================================================== */}
+
+    <span  onClick={goHome} className={style.IconTittle}>
+        <img src={iconImage}/>
+    <h1 className="fontTiltle">PurelyTasty </h1>
+    </span>
+
+<div className={style.ilnksContent
+}>
+<Link className={style.linkNav} to={""}>Library</Link>
+
+
+<Link className={style.linkNav} to={"/contactus"}>Contact Us</Link>
+
+<Link className={style.linkNav} to={"/account"}>Account</Link>
+<div className="dropdown d-block">
+  <button className="btn  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  Restaurant
+  </button>
+  <ul className="dropdown-menu ">
+    <li><Link className="dropdown-item " to="#">Services</Link></li>
+    <li><Link className="dropdown-item" to={"/aboutus"}>About Us</Link></li>
+  </ul>
+</div>
+
+
+</div>
+
+
+
+    
+</div>
+
+
+<div className={style.rightSide}>
+    <button className={style.btnGetApp}>Get The APP</button>
+</div>
+
+    </nav>
+
         </>
     );
 }
